a7e56b714d35cf03d0f4ef1630e1628aeab5111c
Tamires <tamires.ei04@gmail.com>
2025-06-05 17:02:36 -0300

    almost finished

--- Scripts/ui.gd
+++ Scripts/ui.gd
@@ -2,9 +2,12 @@ extends MarginContainer
 
 @export var label_vida_player : Label
 @export var label_vida_inimigo : Label
-@export var label_status_player : Label
-@export var label_status_inimigo : Label
 
+@export var label_energia_player : Label
+@export var label_energia_inimigo : Label
+
+@export var label_status_inimigo : Label
+@export var label_descricao : Label
 
 var enemy_life = Globals.enemy_life
 var enemy_energy = Globals.enemy_energy
@@ -31,7 +34,7 @@ func _ready():
 func _process(_delta):
 	if  player_life <= 0:
 		print("você perdeu")
-		get_tree().paused = true
+		get_tree().change_scene_to_file("res://Scenes/loser.tscn")
 
 	if enemy_life <=0:
 		print("você ganhou")
@@ -39,7 +42,7 @@ func _process(_delta):
 
 	if player_life <= 0 && enemy_life <=0:
 		print("empate")
-		get_tree().paused = true
+		get_tree().change_scene_to_file("res://Scenes/tie.tscn")
 
 func _on_puto_button_up() -> void:
 	print("player escdolheu a carta puto")
@@ -56,6 +59,9 @@ func _on_puto_button_up() -> void:
 	#atualização do label
 	label_vida_inimigo.text = str(enemy_life)
 	label_vida_player.text = str(player_life)
+	
+	label_energia_player.text = str(player_energy)
+	label_energia_inimigo.text = str(enemy_energy)
 	
 func _on_superputo_button_up() -> void:
 	if player_energy >= 3:
@@ -74,6 +80,9 @@ func _on_superputo_button_up() -> void:
 		
 		label_vida_inimigo.text = str(enemy_life)
 		label_vida_player.text = str(player_life)
+		
+		label_energia_player.text = str(player_energy)
+		label_energia_inimigo.text = str(enemy_energy)
 	else:
 		print("você não tem energia o suficiente")
 
@@ -95,6 +104,9 @@ func _on_jarra_button_up() -> void:
 		#atualização do label
 		label_vida_inimigo.text = str(enemy_life)
 		label_vida_player.text = str(player_life)
+		
+		label_energia_player.text = str(player_energy)
+		label_energia_inimigo.text = str(enemy_energy)
 	else:
 		print("você não tem energia o suficiente")
 	
@@ -102,6 +114,10 @@ func _on_comendo_button_up() -> void:
 	player_defense = 0
 	player_life += 6
 	player_energy += 3
+	if player_life > 20:
+		player_life = 20
+	if player_energy > 10:
+		player_energy = 10
 	
 	var random_action = actions[randi() % actions.size()]
 	call(random_action)
@@ -111,6 +127,9 @@ func _on_comendo_button_up() -> void:
 	#atualização do label
 	label_vida_inimigo.text = str(enemy_life)
 	label_vida_player.text = str(player_life)
+	
+	label_energia_player.text = str(player_energy)
+	label_energia_inimigo.text = str(enemy_energy)
 	
 func _on_tuqui_button_up() -> void:
 	if player_energy >= 1:
@@ -129,48 +148,95 @@ func _on_tuqui_button_up() -> void:
 		#atualização do label
 		label_vida_inimigo.text = str(enemy_life)
 		label_vida_player.text = str(player_life)
+		
+		label_energia_player.text = str(player_energy)
+		label_energia_inimigo.text = str(enemy_energy)
 	else:
 		print("você não tem energia o suficiente")
 
 
 func puto():
-	await get_tree().create_timer(1.0).timeout
 	enemy_defense = 0
 	attack_received = 4
+	label_status_inimigo.text = str("The enemy attacked using Puto!")
 	print("inimigo escolheu a carta puto")
 
 func superputo():
-	await get_tree().create_timer(1.0).timeout
 	enemy_defense = -3
 	attack_received = 8
+	label_status_inimigo.text = str("The enemy attacked using SuperPuto!")
 	print("inimigo escolheu a carta superputo")
 
 func jarra():
-	await get_tree().create_timer(1.0).timeout
 	if enemy_energy >= 2:
 		enemy_defense = 4
 		attack_received = 2
 		print("inimigo escolheu a carta jarra")
+		label_status_inimigo.text = str("The enemy attacked using Jarra!")
 	else:
 		var random_action = actions[randi() % actions.size()]
 		call(random_action)
 	
 func comendo():
-	await get_tree().create_timer(1.0).timeout
 	enemy_defense = 0
 	enemy_life += 6
 	enemy_energy += 3
 	attack_received = 0
+	if enemy_life > 20:
+		enemy_life = 20
 	print("inimigo escolheu a carta comendo")
+	if enemy_energy > 10:
+		enemy_energy = 10
+	label_status_inimigo.text = str("The enemy recovered 6 HP and 3 STA\nusing Comendo!")
 	
 func tuqui():
-	await get_tree().create_timer(1.0).timeout
 	if enemy_energy >=1:
 		enemy_defense = 0
 		enemy_energy -=1
 		attack_received = 5
 		enemy_life += -attack_received
+		label_status_inimigo.text = str("The enemy attacked using Tuqui!")
 		print("inimigo escolheu a carta tuqui")
 	else:
 		var random_action = actions[randi() % actions.size()]
 		call(random_action)
+
+
+func _on_puto_mouse_entered() -> void:
+	label_descricao.text = str("PUTO\nA simple attack.\nDamage: 4\nDefense: 0")
+
+
+func _on_superputo_mouse_entered() -> void:
+	label_descricao.text = str("SUPERPUTO\nYour attack is stronger,\nbut you lose your defense.\nDamage: 8\nDefense: -3\nSTA: -3")
+
+
+func _on_jarra_mouse_entered() -> void:
+	label_descricao.text = str("JARRA\nYour defense is stronger,\nbut your attack is weaker.\nDamage: 2\nDefense: 4\nSTA: -2")
+
+
+func _on_comendo_mouse_entered() -> void:
+	label_descricao.text = str("COMENDO\nYou recover your energies.\nSTA: +3\nHP: +6")
+
+
+func _on_tuqui_mouse_entered() -> void:
+	label_descricao.text = str("TUQUI\nYour attack is stronger,\nbut you also take damage.\nSTA: -1\nDamage: 5\nHP: -5")
+
+
+func _on_puto_mouse_exited() -> void:
+	label_descricao.text = str(" ")
+
+
+func _on_superputo_mouse_exited() -> void:
+	label_descricao.text = str(" ")
+
+
+func _on_jarra_mouse_exited() -> void:
+	label_descricao.text = str(" ")
+
+
+func _on_comendo_mouse_exited() -> void:
+	label_descricao.text = str(" ")
+
+
+func _on_tuqui_mouse_exited() -> void:
+	label_descricao.text = str(" ")
